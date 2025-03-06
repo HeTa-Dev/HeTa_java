188d5e6104562eafa9c2197b45a573b4a41628b2
busyforest <22302010017@m.fudan.edu.cn>
2025-03-06 15:43:13 +0800

    fix:修复了聊天功能的bug

--- src/main/java/com/heta/controller/ChatWebSocketHandler.java
+++ src/main/java/com/heta/controller/ChatWebSocketHandler.java
@@ -5,8 +5,6 @@ import com.heta.entity.Message;
 import com.heta.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -31,8 +29,7 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    @PostMapping("/heta/message/saveMessage")
-    protected void handleTextMessage(WebSocketSession session, @RequestBody TextMessage message) throws Exception {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // 解析消息
         Map<String, Object> receivedMessage = objectMapper.readValue(message.getPayload(), Map.class);
         Message chatMessage = objectMapper.convertValue(receivedMessage, Message.class);
@@ -42,21 +39,10 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         // 将消息格式化为JSON
         String formattedMessage = objectMapper.writeValueAsString(chatMessage);
         // 将消息发送给所有连接的客户端
-        // 如果是私信，只发送给指定的接收者
-//        if (chatMessage.isPrivate()) {
-            for (WebSocketSession webSocketSession : sessions) {
-                if (webSocketSession.getAttributes().get("userId").equals(chatMessage.getReceiverId())) {
-                    webSocketSession.sendMessage(new TextMessage(formattedMessage));
-                }
-            }
-//        } else {
-//            // 如果不是私信，广播给所有连接的客户端
-//            for (WebSocketSession webSocketSession : sessions) {
-//                webSocketSession.sendMessage(new TextMessage(formattedMessage));
-//            }
-//        }
+        for (WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(formattedMessage));
+        }
     }
-
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
